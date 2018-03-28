ab5af451aa87ea4772070209aba4b8365d631bb0
aniketlokhande07 <aniketlokhande07@gmail.com>
2018-03-28 16:39:56 +0530

    implemented textfield validation and showing an alert after click on login button

--- DemoGit/ViewController.swift
+++ DemoGit/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate{
 
     @IBOutlet var email: UITextField!
     @IBOutlet var txtpassword: UITextField!
+    
+    var activeTextfiled : UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        email.delegate = self
+        txtpassword.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    // textfield methods
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(textField.text)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     @IBAction func LoginApp(_ sender: Any) {
   
+        if ((email.text?.trimmingCharacters(in: .whitespaces) != nil ) && (txtpassword.text?.trimmingCharacters(in: .whitespaces)) != nil){
+            
+            let alert = UIAlertController(title: "Demo", message: "Login Successfully.", preferredStyle: .alert)
+            let Ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(Ok)
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+        
     
     }
     override func didReceiveMemoryWarning() {
