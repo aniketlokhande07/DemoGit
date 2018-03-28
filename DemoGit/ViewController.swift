1acb954fb03228385ffbc344e8d393f3ad7a9de5
aniketlokhande07 <aniketlokhande07@gmail.com>
2018-03-28 16:09:50 +0530

    1 Embed navigation view controller 2 design login view

--- DemoGit/ViewController.swift
+++ DemoGit/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var email: UITextField!
+    @IBOutlet var txtpassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func LoginApp(_ sender: Any) {
+  
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
